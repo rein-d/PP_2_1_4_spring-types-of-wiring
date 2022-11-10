7fff511083fe2aa6ecb4da0227a1df45f07a5d95
rein_d <room083@gmail.com>
2022-11-10 15:10:23 +0300

    task 2.1.5 - done

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -2,8 +2,14 @@ package koschei.models;
 
 public class Needle7 {
 
+    private final Deth8 deth8;
+
+    public Needle7(Deth8 deth8) {
+        this.deth8 = deth8;
+    }
+
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле " + deth8;
     }
 }
